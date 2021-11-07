e753bd5b70ee4f4b3f39323b014cc5dd5a52b2bf
Oussama Labrahmi <olabrahm@e2r10p11.1337.ma>
2021-11-07 07:26:23 +0100

    XD fixed smoll bug

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: olabrahm <olabrahm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 17:25:36 by olabrahm          #+#    #+#             */
-/*   Updated: 2021/11/07 06:36:21 by olabrahm         ###   ########.fr       */
+/*   Updated: 2021/11/07 07:26:08 by olabrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,14 @@ void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char	*casted_s;
 	unsigned char	to_find;
-	size_t			i;
 
-	i = 0;
-	if (n == 0)
-		return (0);
 	to_find = (unsigned char) c;
 	casted_s = (unsigned char *) s;
-	while (i < n && i < ft_strlen(s))
+	while (n--)
 	{
-		if (casted_s[i] == to_find)
-			return ((void *) &casted_s[i]);
-		i++;
+		if (*casted_s == to_find)
+			return ((void *) casted_s);
+		casted_s++;
 	}
-	if (casted_s[i] == to_find && i < n)
-		return ((void *) &casted_s[i]);
 	return (0);
 }
